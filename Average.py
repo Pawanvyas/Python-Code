4f1ebee5cf1650ec2b558a10da7c6de4be176fa4
Pawanvyas <67539195+Pawanvyas@users.noreply.github.com>
2020-08-29 19:13:16 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,11 @@
+x=int(input("marks in maths is:"))
+y=int(input("marks in science is:"))
+z=int(input("marks in social science is:"))
+v=int(input("marks in english is:"))
+b=int(input("marks in sanskrit is:"))
+a=(x+y+z+v+b)/5
+print("average marks is:",a)
+
+
+    
+    
