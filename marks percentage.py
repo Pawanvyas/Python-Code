5ddf7a5c8dab6bbb922e65e44f7f3abd95dd0694
Pawanvyas <67539195+Pawanvyas@users.noreply.github.com>
2020-08-29 19:08:54 +0530

    Add files via upload

---
+++ marks percentage.py
@@ -0,0 +1,20 @@
+a=input("marks in physics:")
+b=180
+c=float(a)/float(b)*100
+print("percentage in physics {}%".format(c))
+d=input("marks in chemistry:")
+e=float(d)/float(b)*100
+print("percentage in chemistry {}%".format(e))
+f=input("marks in maths:")
+g=float(f)/float(b)*100
+print("percentage in maths {}%".format(g))
+h=input("marks in physical education:")
+i=float(h)/float(b)*100
+print("percentage in physical education {}%".format(i))
+j=input("marks in english:")
+k=float(j)/float(b)*100
+print("percentage in english {}%".format(k))
+l=float(c)+float(e)+float(g)+float(i)+float(k)
+n=500
+m=float(l)/float(n)*100
+print("TOTAL PERCENTAGE {}%".format(m))
